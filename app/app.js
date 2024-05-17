ea46a33dbda05c12fc76ccbc6005fb05da3539f0
Gerald <geraldortiz1693@gmail.com>
2024-05-17 12:46:15 -0500

    app

--- app/app.js
+++ app/app.js
@@ -1,48 +1,11 @@
 const express = require('express');
-const sql = require('mssql');
-const exphbs  = require('express-handlebars');
-
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// const config = {
-//     user: 'admin123',
-//     password: '6EW5SSHR9BVt',
-//     server: 'az-eastus-bsp-dev-newobw-sqls.database.windows.net', 
-//     database: 'az-eastus-bsp-dev-newobw-dbsql',
-//     options: {
-//         encrypt: true 
-//     }
-// };
-
-const config = {
-    user: 'admin123',
-    password: fs.readFileSync('/mnt/secrets-store/dbpassword', 'utf8'),
-    server: 'az-eastus-bsp-dev-newobw-sqls.database.windows.net', 
-    database: 'az-eastus-bsp-dev-newobw-dbsql',
-    options: {
-        encrypt: true 
-    }
-};
-
-sql.connect(config).then(pool => {
-    app.get('/', async (req, res) => {
-        try {
-            const result = await pool.request().query('SELECT TOP 50 * FROM SalesLT.Product');
-            res.render('home', {products: result.recordset});
-        } catch (err) {
-            res.status(500);
-            res.send(err.message);
-        }
-    });
-}).catch(err => {
-    console.error(err);
+app.get('/', (req, res) => {
+	  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
-
+	  console.log(`App listening at http://localhost:${port}`);
+	});
